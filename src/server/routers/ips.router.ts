09c6874b043e97a0954f61c7a2a5ed6836060af4
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-11 22:53:32 +0300

    clean

--- src/server/routers/ips.router.ts
+++ src/server/routers/ips.router.ts
@@ -1,10 +1,8 @@
-import { UnauthorizedError } from '@lib/errors';
-import { authMiddleware } from '@middlewares';
 import { t } from '@trpc-server';
 import { z } from 'zod';
 
 export const ipsRouter = t.router({
-  list: t.procedure.use(authMiddleware).input(z.object({
+  list: t.procedure.input(z.object({
     page: z.number().default(1),
   })).query(async ({ ctx: { db }, input: { page } }) => {
     const users = await db.ips.findMany({
