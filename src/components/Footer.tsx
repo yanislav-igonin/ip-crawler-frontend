62e945deefc0fe1e0f114052c966496def191c9c
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-12 16:48:55 +0300

    change text to eblo ico in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,10 @@ export const Footer = () => <footer className="flex flex-col justify-center item
   <a className="mb-4 flex justify-center items-center" href="https://github.com/yanislav-igonin/ip-crawler-frontend" target='_blank' rel='noreferrer'>
     <GithubIcon />
   </a>
-  <a className='font-medium hover:text-rose-500' href='https://h0b0.dev' target='_blank' rel='noreferrer'>
+  {/* <a className='font-medium hover:text-rose-500' href='https://h0b0.dev' target='_blank' rel='noreferrer'>
     h0b0
+  </a> */}
+  <a className='animate-spin' href='https://h0b0.dev' target='_blank' rel='noreferrer'>
+    <Image src='/eblo.ico' alt='h0b0.dev' width={40} height={40} />
   </a>
 </footer>;
