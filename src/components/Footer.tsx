3f515282799f45026089392186afb137ec39a59b
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-12 16:52:42 +0300

    remove footer height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { GithubIcon } from './GithubIcon';
 
-export const Footer = () => <footer className="flex flex-col justify-center items-center w-full h-10 bottom-0 mb-4">
+export const Footer = () => <footer className="flex flex-col justify-center items-center w-full bottom-0 mb-4">
   <a className="mb-4 flex justify-center items-center" href="https://github.com/yanislav-igonin/ip-crawler-frontend" target='_blank' rel='noreferrer'>
     <GithubIcon />
   </a>
