d467c153852ec26447344c34ea3a78d9e8f6ce2f
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-12 16:45:20 +0300

    add link to main site

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
 import { GithubIcon } from './GithubIcon';
 
 export const Footer = () => <footer className="flex flex-col justify-center items-center w-full h-10 bottom-0 mb-4">
-  <a className="flex justify-center items-center" href="https://github.com/yanislav-igonin/ip-crawler-frontend" target='_blank' rel='noreferrer'>
+  <a className="mb-4 flex justify-center items-center" href="https://github.com/yanislav-igonin/ip-crawler-frontend" target='_blank' rel='noreferrer'>
     <GithubIcon />
+  </a>
+  <a className='font-medium hover:text-rose-500' href='https://h0b0.dev' target='_blank' rel='noreferrer'>
+    h0b0
   </a>
 </footer>;
