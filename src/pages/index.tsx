eb3b3c006ff30b8504065950889f936de49b3d43
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-01 22:22:48 +0200

    fix hydration error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ const Index: NextPage = () => {
   const onPaginationChange = (page: number) => {
     setActivePage(page);
   };
-  
+
   useEffect(() => {
     router.push(`/?page=${activePage}`, undefined, { shallow: true });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activePage]);
 
   const { data: ipsData } = trpc.ips.list.useQuery({ page: activePage });
@@ -33,47 +33,49 @@ const Index: NextPage = () => {
     <main className='w-screen p-4'>
       <div className="text-center">
         <Heading>Checked IPs: {countAllData ? countAllData.count : 'loading...'}</Heading>
-        <Heading>Live IPs: {countLiveData? countLiveData.count : 'loading...'}</Heading>
+        <Heading>Live IPs: {countLiveData ? countLiveData.count : 'loading...'}</Heading>
       </div>
-      <table className='table-fixed mb-4 w-full dark:text-slate-200'>
-        <thead>
-          <tr>
-            <th className='px-4 py-2'>IP</th>
-            <th className='px-4 py-2'>Check Date</th>
-          </tr>
-        </thead>
 
-        <tbody>
-          {!ipsData
-            ? <div className="flex justify-center items-center w-screen h-screen">
-              <Spinner />
-            </div>
-            : ipsData.ips.map((ip) => <tr key={ip.address}>
-              <td className='border border-slate-300 dark:border-slate-400 px-4 py-2 text-center'>
-                <a className='text-lg hover:text-rose-500 visited:text-rose-700'
-                  href={`http://${ip.address}`} target='_blank' rel='noreferrer'>
-                  {ip.address}
-                </a>
-              </td>
-              <td className='text-lg border border-slate-300 dark:border-slate-400 px-4 py-2 text-center'>
-                {ip.checkedAt.toLocaleDateString()}
-              </td>
-            </tr>)}
-        </tbody>
-      </table>
+      {!ipsData
+        ? <div className="flex justify-center items-center w-screen h-screen">
+          <Spinner />
+        </div>
+        : <>
+          <table className='table-fixed mb-4 w-full dark:text-slate-200'>
+            <thead>
+              <tr>
+                <th className='px-4 py-2'>IP</th>
+                <th className='px-4 py-2'>Check Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {ipsData.ips.map((ip) => <tr key={ip.address}>
+                <td className='border border-slate-300 dark:border-slate-400 px-4 py-2 text-center'>
+                  <a className='text-lg hover:text-rose-500 visited:text-rose-700'
+                    href={`http://${ip.address}`} target='_blank' rel='noreferrer'>
+                    {ip.address}
+                  </a>
+                </td>
+                <td className='text-lg border border-slate-300 dark:border-slate-400 px-4 py-2 text-center'>
+                  {ip.checkedAt.toLocaleDateString()}
+                </td>
+              </tr>)}
+            </tbody>
+          </table>
 
-      <ul className='flex justify-center items-center gap-2 flex-wrap dark:text-slate-200'>
-        {pages.map((page) => <li key={page}>
-          <p onClick={() => onPaginationChange(page)}
-            className={`${page === activePage ? 'text-xl' : 'text-lg'} cursor-pointer`}
-            style={{
-              color: page === activePage ? 'rgb(244 63 94)' : 'inherit',
-              fontWeight: page === activePage ? 'bold' : 'inherit',
-            }}>
-            {page}
-          </p>
-        </li>)}
-      </ul>
+          <ul className='flex justify-center items-center gap-2 flex-wrap dark:text-slate-200'>
+            {pages.map((page) => <li key={page}>
+              <p onClick={() => onPaginationChange(page)}
+                className={`${page === activePage ? 'text-xl' : 'text-lg'} cursor-pointer`}
+                style={{
+                  color: page === activePage ? 'rgb(244 63 94)' : 'inherit',
+                  fontWeight: page === activePage ? 'bold' : 'inherit',
+                }}>
+                {page}
+              </p>
+            </li>)}
+          </ul>
+        </>}
     </main>
   </Layout>;
 };
